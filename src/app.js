cf8aebc3b80a271b0217e98ca23c249b4df41245
Juan Ramon Vidal <jvidalpar@uoc.edu>
2024-11-10 08:29:35 +0100

    Primera versión totalmente funcional del proyecto

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
+import "./styles.scss";
+
 const name = 'world';
 console.log(`Hello ${name}`);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const imagenes = document.querySelectorAll('.imagen-interactiva');
+  
+    imagenes.forEach(function(imagen) {
+      imagen.addEventListener('mouseenter', function() {
+        imagen.style.transform = 'scale(1.1)';
+      });
+  
+      imagen.addEventListener('mouseleave', function() {
+        imagen.style.transform = 'scale(1)';
+      });
+    });
+  });
+  
+  
+  
