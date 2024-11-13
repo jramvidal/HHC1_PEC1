c944cd787458f22642a8df2ae181acc95779dc19
Juan Ramon Vidal <jvidalpar@uoc.edu>
2024-11-13 19:52:29 +0100

    Versión definitiva

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-import "./styles.scss";
+import "./styles.scss"; // Importa los estilos principales
 
+document.addEventListener("DOMContentLoaded", function () {
+  const imagenes = document.querySelectorAll(".imagen-interactiva"); // Selecciona todas las imágenes interactivas
 
-document.addEventListener('DOMContentLoaded', function() {
-    const imagenes = document.querySelectorAll('.imagen-interactiva');
-  
-    imagenes.forEach(function(imagen) {
-      imagen.addEventListener('mouseenter', function() {
-        imagen.style.transform = 'scale(1.1)';
-      });
-  
-      imagen.addEventListener('mouseleave', function() {
-        imagen.style.transform = 'scale(1)';
-      });
+  // Añade efectos de zoom al pasar el cursor sobre cada imagen
+  imagenes.forEach(function (imagen) {
+    imagen.addEventListener("mouseenter", function () {
+      imagen.style.transform = "scale(1.1)"; // Aplica un aumento al pasar el cursor
+    });
+
+    imagen.addEventListener("mouseleave", function () {
+      imagen.style.transform = "scale(1)"; // Restaura el tamaño original al quitar el cursor
     });
   });
-  
-  
-  
+});
